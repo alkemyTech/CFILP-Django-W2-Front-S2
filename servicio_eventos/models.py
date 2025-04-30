ee4fc19d0b145c9822db1cbb2fb0a3f499e49cf8
Joaquin <joaquinhernandezcacu@gmail.com>
2025-04-30 17:52:13 -0300

    create models servicio_eventos

--- servicio_eventos/models.py
+++ servicio_eventos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    activo = models.BooleanField(default=True)
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    numero_legajo = models.IntegerField()
+    activo = models.BooleanField(default=True)
+
+class Coordinador(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    numero_documento = models.IntegerField()
+    fecha_alta = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    activo = models.BooleanField(default=True)
