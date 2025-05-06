924f09024a1f9da2a21da5e053d452842c439b82
Joaquin <joaquinhernandezcacu@gmail.com>
2025-05-06 19:57:25 -0300

    test joa

--- servicio_eventos/views.py
+++ servicio_eventos/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 """ views.py 
 
 Toda la logica como intermediario entre el modelo y la vista se encuentra en este archivo.
-
+# hola
 
 """
 def view_home(request):
