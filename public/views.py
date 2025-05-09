757eadad4d3ddc6eb98624f3aa05dd8ef75264b6
Joaquin <joaquinhernandezcacu@gmail.com>
2025-05-09 02:37:23 -0300

    Implementacion id en cotizacion personalizada

--- public/views.py
+++ public/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render
-from servicio_eventos.models import Cliente,Servicio
+from servicio_eventos.models import Cliente, Servicio
+from django.shortcuts import render, get_object_or_404
 
 def view_casa(request):
     return render(request, "public/casa.html")
 
 def view_cotizacion(request):
+    evento = None
+
+    # Verificar si la solicitud es GET y si existe el parámetro 'evento_id'
+    if request.method == "GET":
+        evento_id = request.GET.get("evento_id")
+        if evento_id:
+            evento = get_object_or_404(Servicio, id=evento_id)  # Obtener evento por su ID
+        servicios = Servicio.objects.all()  # Obtener los servicios desde la base de datos
+
+        return render(request, "public/cotizacion.html", {
+            "evento": evento,
+            "servicios": servicios
+        })
+
+    # Verificar si la solicitud es POST
     if request.method == "POST":
         nombre = request.POST.get("nombre")
         apellido = request.POST.get("apellido")
         email = request.POST.get("email") or request.data.get('email')
         telefono = request.POST.get("telefono")
         Cliente.objects.create(nombre=nombre, apellido=apellido, email=email, telefono=telefono, activo=True)
-        
+
         print("Se hizo un POST")
         # Aquí puedes agregar la lógica para guardar los datos o enviar un correo
-    
-    if request.method == "GET":
-        servicios = Servicio.objects.all()  # Obtienes los servicios desde la base de datos
-        return render(request, "public/cotizacion.html", {"servicios": servicios})
-    
+
     return render(request, "public/cotizacion.html")
     
 
